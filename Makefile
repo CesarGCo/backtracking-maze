e7db69e9ce6d472cdd73f12501dd1965893db760
ViniciusOliveira2004 <vinicius.oliveira3@ufv.br>
2024-12-02 19:42:18 -0300

    feat: Adicionando conteúdo ao Makefile e função main

---
+++ Makefile
@@ -0,0 +1,3 @@
+All: main.c fileReader/fileReader.c
+	gcc main.c -o exec fileReader/fileReader.c
+Run:./exec
--- main.c
+++ main.c
@@ -1,7 +1,8 @@
+#include "common.h"
 #include "./fileReader/fileReader.h"
 
 int main() {
-
+    printf("Hello World\n");
     // F no chat
     return 0;
 }
