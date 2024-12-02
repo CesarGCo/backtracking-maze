1018300d9b9fb5b49bbef571eafd4c87df25ff4e
ViniciusOliveira2004 <vinicius.oliveira3@ufv.br>
2024-12-02 19:42:50 -0300

    feat: Adição do arquivo common.h

---
+++ common.h
@@ -0,0 +1,16 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+
+// Definindo cores utilizadas no código:
+#define RED "\x1b[31m"
+#define WHITE "\x1b[37m"
+#define YELLOW "\x1B[33m"
+#define BLUE "\x1B[34m"
+#define GREEN "\e[0;32m"
+
+//Incluindo Bibliotecas padrão:
+#include <stdlib.h>
+#include <stdio.h>
+
+#endif
