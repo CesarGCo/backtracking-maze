1381fbc09ad643a0dc40f0a887a816c3ffbe6c43
César Grandis Costa <cesargrandis38@gmail.com>
2024-12-02 23:33:46 -0300

    feat: implementação do leitor de arquivos

---
+++ fileReader/fileReader.c
@@ -0,0 +1,40 @@
+#include "fileReader.h"
+
+DadosLidos le_arquivo(char *caminho){
+    DadosLidos dado;
+    FILE *arquivo = fopen(caminho, "r");
+
+    if(arquivo == NULL){
+        printf("Failed to open file\n");
+        dado.num_chaves = 0;
+        dado.matriz_labirinto = NULL;
+        return dado;
+    }
+
+    
+    fscanf(arquivo, "%d %d %d", &dado.tamanho_linha, &dado.tamanho_coluna, &dado.num_chaves);
+
+    
+    dado.matriz_labirinto = (int **)malloc(dado.tamanho_linha * sizeof(int *));
+    for(int i = 0; i < dado.tamanho_linha; i++){
+        dado.matriz_labirinto[i] = (int *)malloc(dado.tamanho_coluna * sizeof(int));
+    }
+
+    for(int i = 0; i < dado.tamanho_linha; i++){
+        for(int j = 0; j < dado.tamanho_coluna; j++){
+            fscanf(arquivo, "%1d", &dado.matriz_labirinto[i][j]);
+        }
+    }
+
+    fclose(arquivo);
+    return dado;
+}
+
+
+void libera_matriz(DadosLidos *dado) {
+    for (int i = 0; i < dado->tamanho_linha; i++) {
+        free(dado->matriz_labirinto[i]);
+    }
+    free(dado->matriz_labirinto);
+    dado->matriz_labirinto = NULL;
+}
--- fileReader/fileReader.h
+++ fileReader/fileReader.h
@@ -1,5 +1,18 @@
 #ifndef FILE_READER
+#define FILE_READER
 
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct {
+    int num_chaves;
+    int **matriz_labirinto;
+    int tamanho_linha;
+    int tamanho_coluna;
+} DadosLidos;
+
+DadosLidos le_arquivo(char *caminho);
+
+void libera_matriz(DadosLidos *dado);
 
 #endif
