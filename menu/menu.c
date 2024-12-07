25d0bf89ebc54f46416468ddec6c0e04938fe821
César Grandis Costa <cesargrandis38@gmail.com>
2024-12-07 15:57:46 -0300

    feat: adição do quarto caso no menu

--- menu/menu.c
+++ menu/menu.c
@@ -16,11 +16,18 @@ void exibir_menu(int isAnalise) {
     int verifica_back = 0;
     int **copia_labirinto = NULL;
 
+    //Variaveis do gerador de labirinto
+    int numero_linhas;
+    int numero_colunas;
+    int numero_chaves_chao;
+    int numero_chaves_inicial;
+    int numero_portas;
+
     do {
         #ifdef OS_Windows
-            system("cls");
+            //system("cls");
         #else
-            system("clear");
+            //system("clear");
         #endif 
 
         menu(isAnalise);
@@ -68,11 +75,48 @@ void exibir_menu(int isAnalise) {
                 esperar_usuario();
                 break;
 
+            case 4:
+                printf("| Digite a quantidade de linhas do labirinto: ");
+                scanf("%d", &numero_linhas);
+                printf("| Digite a quantidade de colunas do labirinto: ");
+                scanf("%d", &numero_colunas);
+                printf("| Digite a quantidade de chaves iniciais do estudante: ");
+                scanf("%d", &numero_chaves_inicial);
+                printf("| Digite a quantidade de chaves que estarao no chao: ");
+                scanf("%d", &numero_chaves_chao);
+                printf("| Digite a quantidade de portas presentes no labrinto: ");
+                scanf("%d", &numero_portas);
+
+                create_maze(&dado, numero_linhas, numero_colunas, numero_chaves_chao, numero_chaves_inicial, numero_portas);
+                printf("| Labirinto gerado abaixo: \n");
+                exibe_labirinto(dado.matriz_labirinto, dado.tamanho_linha, dado.tamanho_coluna);
+                printf("| Se deseja usar o labirinto digite (1) caso contrario digite (2): ");
+                int opcao_digitada = 0;
+                scanf("%d", &opcao_digitada);
+                if(opcao_digitada == 1){
+                    copia_labirinto = copia_matriz(dado.matriz_labirinto, dado.tamanho_linha, dado.tamanho_coluna);
+                    int encontrou_saida = 0;
+                    inicia_backtracking(dado, isAnalise, copia_labirinto, &encontrou_saida);
+                    if (!encontrou_saida) {
+                        printf("| O labirinto " RED "NAO" RESET " possui saída!\n");
+                    } else {
+                        printf("| Backtracking executado com " GREEN "SUCESSO" RESET ".\n");
+                    }
+                    verifica_back = 1;
+                    esperar_usuario();
+                } else if(opcao_digitada == 2){
+                    continue;
+                }
+                break;
+
+
+
             default:
                 printf("| Saindo...\n");
                 exit(0);
         }
     } while (1);
+
 }
 
 void menu(int isAnalise) {
@@ -86,6 +130,7 @@ void menu(int isAnalise) {
     printf("|   1 - Carregar novo arquivo de dados  |\n");
     printf("|   2 - Processar e exibir resposta     |\n");
     printf("|   3 - Mostrar labirinto               |\n");
+    printf("|   4 - Gerar labirinto                 |\n");
     printf("|   Outro - Sair                        |\n");
     printf("|=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=|\n");
     printf("| >>> ");
--- menu/menu.h
+++ menu/menu.h
@@ -1,6 +1,7 @@
 #include "../common.h"
 #include "../fileReader/fileReader.h"
 #include "../backtracking/backtracking.h"
+#include "../mazeGenerator/maze_generator.h"
 
 #ifndef MENU
 #define MENU
